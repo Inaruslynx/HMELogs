9fa37d6692d4f9caaa410f051a156734699a1308
Joshua Edwards <inaruslynx@gmail.com>
2023-12-06 11:15:51 -0600

    Lots of updates

---
+++ src/public/js/cbUnhider.js
@@ -0,0 +1,3 @@
+function cbUnhider(params) {
+    
+}
---
+++ src/public/js/main.js
@@ -0,0 +1,16 @@
+// This script just makes it easier for me to import all of my scripts into the page
+$(document).ready(function () {
+  const url = "http://fs3s-hotmilllog/HM_Walkthrough/js/";
+  const ListOfScripts = [
+    "DynamicScrollspy.js",
+    "getCookie.js",
+    "toggleMode.js",
+    "validateForms.js",
+  ];
+
+  ListOfScripts.forEach((element) => {
+    $.getScript(`${url}${element}`, function (script, textStatus, jqXHR) {
+      console.log(`Script: ${element} loaded`);
+    });
+  });
+});
