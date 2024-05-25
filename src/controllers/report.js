b3df8567c399e4ef8ccde7ce9626d990c3901bf1
Inaruslynx <inaruslynx@gmail.com>
2024-05-25 02:41:00 -0500

    Updated Report to include concerning items

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -4,12 +4,17 @@ const { std, mean, min, max, round } = require("mathjs");
 module.exports.getReport = async (req, res) => {
   const results = {};
   const resultsOfRecentLogs = {};
+  const itemsOfConcern = {};
+
   // get all logs which will be an array of objects
   const logs = await Log.find().select("data -_id").exec();
+
   let refinedLogs = logs.map((log) => {
     //console.log(Object.entries(log.data));
     return log.data;
   });
+
+  //refinedLogs will be just data with only numbers
   refinedLogs = refinedLogs.map((data) => {
     //console.log(data)
     let result = Object.fromEntries(
@@ -20,25 +25,23 @@ module.exports.getReport = async (req, res) => {
     );
     return result;
   });
+
+  // We pop the last log off into it's own object
   const lastLog = refinedLogs.pop();
+
+  // We get the long before the last yet don't remove it from refinedLogs
   const beforeLastLog = refinedLogs[refinedLogs.length - 1];
 
   // Iterate over each key in lastLog
   Object.keys(lastLog).forEach((key) => {
     // Check if the key exists in beforeLastLog
     if (beforeLastLog.hasOwnProperty(key)) {
-      // Calculate the difference and store it in the results object
+      // Calculate the difference and store it in the resultsOfRecentLogs object
       resultsOfRecentLogs[key] = round(lastLog[key] - beforeLastLog[key], 2);
     }
   });
   // console.log(resultsOfRecentLogs);
 
-  // lastLog = Object.fromEntries(
-  //   Object.entries(lastLog).filter(
-  //     ([key, value]) =>
-  //       typeof parseFloat(value) === "number" && !isNaN(parseFloat(value))
-  //   )
-  // );
   // console.log(refinedLogs);
   Object.keys(refinedLogs[0]).forEach((key) => {
     let values = refinedLogs.map((data) => parseFloat(data[key]));
@@ -54,7 +57,30 @@ module.exports.getReport = async (req, res) => {
       Max,
     };
   });
+
+    // Compare lastLog values against calculated statistics
+    Object.keys(lastLog).forEach(key => {
+      if (results.hasOwnProperty(key)) {
+        const value = parseFloat(lastLog[key]);
+        const { Min, Max, stdDev } = results[key];
+        const recentLogDiff = resultsOfRecentLogs[key];
+
+        const isAboveMax = value > Max;
+        const isBelowMin = value < Min;
+        const isAboveStdDev = Math.abs(recentLogDiff) > stdDev;
+        
+        if (isAboveMax || isBelowMin || isAboveStdDev) {
+          itemsOfConcern[key] = {
+            aboveMax: isAboveMax,
+            belowMin: isBelowMin,
+            aboveStdDev: isAboveStdDev
+          };
+        }
+      }
+    });
+  console.log(itemsOfConcern)
+  
   // console.log(lastLog);
   // This will render the page
-  res.render("reports/page", { lastLog, beforeLastLog, results, resultsOfRecentLogs });
+  res.render("reports/page", { lastLog, beforeLastLog, results, resultsOfRecentLogs, itemsOfConcern });
 };
